5fcc499d6de899f069c044f34a9b0a132dee2fb5
Wa <122730917+STKR2@users.noreply.github.com>
2024-01-23 02:23:17 +0300

    Update fifthon.py

--- fifthon.py
+++ fifthon.py
@@ -45,7 +45,7 @@ time_bio = ["off"]
 
 async def join_channel():
     try:
-        await fifthon(JoinChannelRequest("@fifthon"))
+        await fifthon(JoinChannelRequest("@RR74BOT"))
     except BaseException:
         pass
 
