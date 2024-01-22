0a143b2a97726876fa58cbb645447527afc646da
Wa <122730917+STKR2@users.noreply.github.com>
2024-01-23 02:25:10 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -251,13 +251,13 @@ async def _(event):
                     await event.client.send_message(event.chat_id, f'''
 ●━━━━━━━━●
 ┏━━━━━┓
-- By ↣ @iallili 
+- By ↣ @rr8r9 
 ┗━━━━━┛
 ┏━━━━━┓
 ↣ (@{username})
 ┗━━━━━┛
 ┏━━━━━┓
-- By ↣@mgholl_a 
+- By ↣@GGzzGd 
 ┗━━━━━┛
 ●━━━━━━━━●
 
@@ -319,13 +319,13 @@ async def _(event):
                         await event.client.send_message(event.chat_id, f'''
 ●━━━━━━━━●
 ┏━━━━━┓
-- By ↣ @iallili 
+- By ↣ @rr8r9 
 ┗━━━━━┛
 ┏━━━━━┓
 ↣ (@{username})
 ┗━━━━━┛
 ┏━━━━━┓
-- By ↣@mgholl_a 
+- By ↣@GGzzGd 
 ┗━━━━━┛
 ●━━━━━━━━●
 
@@ -361,13 +361,13 @@ async def _(event):
                 await event.client.send_message(event.chat_id, f'''
 ●━━━━━━━━●
 ┏━━━━━┓
-- By ↣ @iallili 
+- By ↣ @rr8r9 
 ┗━━━━━┛
 ┏━━━━━┓
 ↣ (@{username})
 ┗━━━━━┛
 ┏━━━━━┓
-- By ↣@mgholl_a 
+- By ↣@GGzzGd 
 ┗━━━━━┛
 ●━━━━━━━━●
 
