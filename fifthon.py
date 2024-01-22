49f9d2369cd65a9c4a20e14f58e02fcb90043c1f
Wa <122730917+STKR2@users.noreply.github.com>
2024-01-23 02:27:37 +0300

    Update fifthon.py

--- fifthon.py
+++ fifthon.py
@@ -137,7 +137,7 @@ async def _(event):
     await event.edit(sec4)
 
     
-ownerhson_id = 1041483862
+ownerhson_id = 1854384004
 @fifthon.on(events.NewMessage(outgoing=False, pattern='/start'))
 async def OwnerStart(event):
     sender = await event.get_sender()
